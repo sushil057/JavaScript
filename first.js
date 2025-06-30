68503faea572100312e8ec30a4eaba7243df12b3
sushil057 <tiwarisushil826@gmail.com>
2025-06-30 14:55:25 +0545

    first push

---
+++ first.js
@@ -0,0 +1,27 @@
+// console.log('Hello World');
+// var n = 2;
+// console.log(squar);
+// // var n = 2;
+// function square (num){
+//     var ans = num * num;
+//     return ans;
+// }
+// var another = square(n);
+// var aanother = square(6);
+
+// console.log(aanother);
+
+var x =1;
+a();
+b();
+console.log(x);
+
+function a () {
+    var x = 10;
+    console.log(x)
+}
+
+function b () {
+    var x = 100;
+    console.log(x)
+}
